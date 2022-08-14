d5ce580f59e3fae31976f8c96cb75dd20caba08e
Khushal-ag <starkhush5@gmail.com>
2022-08-14 18:29:06 +0530

    initial commit

---
+++ 412.fizz-buzz.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=412 lang=java
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            res.add(i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : "" + i));
+        }
+        return res;
+    }
+}
+// @lc code=end
+
