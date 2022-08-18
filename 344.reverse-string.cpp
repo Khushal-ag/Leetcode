552923033a390b4300b0bb2a1e2ca6acbab8126d
Khushal-ag <starkhush5@gmail.com>
2022-08-18 10:36:24 +0530

    replace swap function

--- 344.reverse-string.cpp
+++ 344.reverse-string.cpp
@@ -10,9 +10,7 @@ public:
     void reverseString(vector<char>& s) {
         for (int i = 0; i < s.size() / 2;i++)
         {
-            auto temp = s[i];
-            s[i] = s[s.size() - i - 1];
-            s[s.size() - i - 1] = temp;
+            swap(s[i], s[s.size() - i - 1]);
         }
     }
 };
