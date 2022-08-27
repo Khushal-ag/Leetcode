21e3bb3ccb0deff49acaa76738b094095b1d25a7
Khushal-ag <starkhush5@gmail.com>
2022-08-27 19:38:31 +0530

    initial commit

---
+++ 35.search-insert-position.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, h = nums.size(), mid,minindex = nums.size()-1;
+        if(target>nums[h-1])
+            return h;
+        else if (target < nums[l])
+            return l;
+        while(l<=h)
+        {
+            mid = l + (h - l) / 2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+            {
+                h = mid - 1;
+                minindex = min(minindex, mid);
+            }
+            else
+                l = mid + 1;
+        }
+        return minindex;
+    }
+};
+// @lc code=end
+
