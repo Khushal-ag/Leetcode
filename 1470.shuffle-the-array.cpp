8693943e87ed1c0ee27a4f2547b01032bc9249a9
Khushal-ag <starkhush5@gmail.com>
2022-08-28 22:53:23 +0530

    initial commit

---
+++ 1470.shuffle-the-array.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1470 lang=cpp
+ *
+ * [1470] Shuffle the Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> v;
+        for (int i = 0; i<n;i++)
+        {
+            v.push_back(nums[i]);
+            v.push_back(nums[i+n]);
+        }
+        return v;
+    }
+};
+// @lc code=end
+
