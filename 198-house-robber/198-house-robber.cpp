88942b927f153fc6fe7e269ef4e5483e8c0221d3
Khushal_Agarwal <starkhush5@gmail.com>
2022-09-17 11:10:58 +0530

    Time: 10 ms (7.75%), Space: 9.6 MB (6.20%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        return fun(dp,nums,nums.size()-1);
+    }
+    int fun(vector<int>&dp,vector<int> a,int ind)
+    {
+        if(ind == 0) return a[0];
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int take = a[ind] + fun(dp,a,ind-2);
+        int notake = 0 + fun(dp,a,ind-1);
+        
+        return dp[ind]=max(take,notake);
+    }
+};
