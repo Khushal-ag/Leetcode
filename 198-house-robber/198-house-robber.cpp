999178257f994851962160fb87257058a50f5fee
Khushal_Agarwal <starkhush5@gmail.com>
2022-09-17 11:17:35 +0530

    Time: 2 ms (58.93%), Space: 7.7 MB (49.60%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    // Memoization solution
-    int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
-        return fun(dp,nums,nums.size()-1);
-    }
-    int fun(vector<int>&dp,vector<int> a,int ind)
-    {
-        if(ind == 0) return a[0];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int take = a[ind] + fun(dp,a,ind-2);
-        int notake = 0 + fun(dp,a,ind-1);
-        
-        return dp[ind]=max(take,notake);
+    // Tabulation solution + space optimised O(1)
+    int rob(vector<int>& a) {
+        int n = a.size();
+        int prev = a[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++)
+        {
+            int take = a[i];
+            if(i>1) take += prev2;
+            int notake = 0 + prev;
+            int curr = max(take,notake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
