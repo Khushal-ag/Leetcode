cfdfd05aa84f307079d20f51c3b99f62aa72d56b
Khushal_Agarwal <starkhush5@gmail.com>
2022-09-24 11:32:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.56%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while(n/5 > 0)
+        {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+};
