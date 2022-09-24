e87225954039748f50c29279ff176d1548c55139
Khushal_Agarwal <starkhush5@gmail.com>
2022-09-24 15:42:13 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (49.72%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     // Tabulation solution + space optimised O(1)
+    int chk(vector<int>& a,int n,vector<int>& dp)
+    {
+        if(n==0) return a[0];
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int take = a[n] + chk(a,n-2,dp);
+        int notake = chk(a,n-1,dp);
+        return dp[n] = max(take,notake);
+    }
     int rob(vector<int>& a) {
         int n = a.size();
-        int prev = a[0];
-        int prev2 = 0;
-        for(int i=1;i<n;i++)
-        {
-            int take = a[i];
-            if(i>1) take += prev2;
-            int notake = 0 + prev;
-            int curr = max(take,notake);
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        vector<int> dp(n+1,-1);
+        return chk(a,n-1,dp);
     }
 };
