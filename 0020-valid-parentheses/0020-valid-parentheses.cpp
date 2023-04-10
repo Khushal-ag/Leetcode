be5cab524c98a5b5f1b34f07e44ad27220a6ed80
Khushal_Agarwal <starkhush5@gmail.com>
2023-04-10 19:41:52 +0530

    Time: 2 ms (46.33%), Space: 6.5 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for (int i = 0; i < s.size();i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else{
+                if (st.empty() || (st.top() == '(' && s[i] != ')') || (st.top() == '{' && s[i] != '}') || (st.top() == '[' && s[i] != ']'))
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
